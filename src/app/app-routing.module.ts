98f7c32525bef969040a258a348558ae58633e37
jota santos <gestald118@email.com>
2024-06-23 20:14:55 -0400

    está dando certo o layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
   },
   {
-    path: 'heroes',
+    path: 'users',
     loadChildren: () => import('./users/users.module').then( m => m.UsersModule ),
   },
 ];
