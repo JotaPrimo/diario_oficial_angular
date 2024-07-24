4d782a399f9fbd26964c77e7ff3143ec03ce27c4
Jota Santos <jailsonsantosdpge@outlook.com>
2024-07-24 19:00:00 -0400

    registrando intereptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { TokenService } from './auth/services/token.service';
+import { AuthInterceptor } from './auth/interceptors/auth.interceptor ';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import { TokenService } from './auth/services/token.service';
   providers: [
     CookieService,
     TokenService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
