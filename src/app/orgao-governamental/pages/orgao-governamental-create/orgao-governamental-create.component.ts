643f99dd222981a36b9603b4f99c722f0b26f7ac
jota santos <gestald118@email.com>
2024-10-09 07:59:07 -0400

    Criando controles do form de create

--- src/app/orgao-governamental/pages/orgao-governamental-create/orgao-governamental-create.component.ts
+++ src/app/orgao-governamental/pages/orgao-governamental-create/orgao-governamental-create.component.ts
@@ -11,27 +11,45 @@ import { valorNaListaValidator } from '../../../shared/custon_validators';
 })
 export class CreateComponent implements OnInit {
 
-  private tiposOrgaos: string[] = ['Municipal', 'Estadual'];
+  private tiposOrgaos: string[] = ['Prefeitura Municipal', 'Governo Estadual'];
 
-  public form: FormGroup = this.formBuilder.group({
-    name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255)]],
-    cnpj: ['', [Validators.required, ]]
-  });
+  public form: FormGroup = new FormGroup({});
 
   constructor(
     private formBuilder: FormBuilder
   ) { }
 
+  // ngOnInit é clico de vida correto para inicialiar componentes
+  // que não depende de injeção no construtor
   ngOnInit() {
+    this.inicializarForm();
+    this.onChangeSelectTipo();
   }
 
   getTiposOrgaos(): string[] {
     return this.tiposOrgaos;
   }
 
-  handleSave() {
-
+  inicializarForm() {
+    this.form = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(255)]],
+      cnpj: ['', [Validators.required]],
+      tipo: ['', [Validators.required]]
+    });
   }
 
+  handleSave() {
+  }
+
+  onChangeSelectTipo(): void {
+    this.form.get('tipo')?.valueChanges.subscribe((value) => {
+      this.construindoNome(value);
+    });
+  }
+
+  /** Método para setar automaticamete o valor */
+  construindoNome(novoValor: string): void {
+      this.form.get('nome')?.setValue(`${novoValor} `);
+  }
 
 }
