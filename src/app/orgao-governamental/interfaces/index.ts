dc7affb0e5a00d133190d2ef001dc78908312043
jota santos <gestald118@email.com>
2024-10-11 12:39:53 -0400

    Renomeando interfaces

---
+++ src/app/orgao-governamental/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './orgao-governamental.interface';
+export * from './orgao-governamental-create-dt.interface';
+export * from './orgao-governamental-response.interface';
---
+++ src/app/orgao-governamental/interfaces/orgao-governamental.interface.ts
@@ -0,0 +1,12 @@
+export interface OrgaoGovernamental {
+  id: number,
+  nome: string,
+  cnpj: string,
+  created_at: string,
+  updated_at: string,
+}
+
+
+
+
+
